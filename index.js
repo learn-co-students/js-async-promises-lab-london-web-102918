6b8a5ac957da446127bea8395a6a5b30caced2c0
rosen777 <rosen.toshev@flatironschool.com>
2018-12-13 19:16:05 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,50 @@ const questions = [
 ]
 
 let question;
+let btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+
+let questionContainer = document.querySelector('.question-container')
+
+ const appendQuestion = (question) => {
+   questionContainer.innerHTML = question.questionText.trim()
+ }
+
+const askQuestionThen = (time) => {
+  question = questions[0]
+  return new Promise((resolve, reject) => {
+          setTimeout(function() {
+            appendQuestion()
+            resolve(question)
+          }, time )
+      })
+  }
+
+const removeQuestion = () => {
+  questionContainer.innerHTML = '';
+}
+
+const askQuestionThenRemoveQuestion = (time) => {
+  const questionT = question['questionText']
+   questionContainer.innerText = questionT
+
+   question = questions[0]
+   return new Promise(() => {
+     setTimeout(function(){
+     }, time)
+   })
+   .then(response => questionContainer.innerText = '')
+}
+
+const trueAndFalseButtons = () => {
+return btns
+}
+
+const toggleTrueAndFalseButtons = () => {
+  trueAndFalseButtons().forEach(function(btn) {
+  btn.classList.toggle('hide')
+})
+}
+
+const displayQuestionOnClick = (time) => {
+  return askQuestionThen(time).then(removeQuestion)
+}
