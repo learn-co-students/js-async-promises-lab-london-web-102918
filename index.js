402c00f8e46b2b24141da51d2f70a6c5e6de6073
artismarti <artismarti@gmail.com>
2018-12-13 15:34:40 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,51 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question) {
+  document.getElementById('question-container').innerText = question.questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  let promise =  new Promise(function(resolve, reject){
+    setTimeout(function(){
+      resolve()
+    },time)
+})
+  return promise
+}
+
+function removeQuestion() {
+  document.getElementById('question-container').innerText = ''
+}
+
+function trueAndFalseButtons() {
+  let btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  return btns
+}
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick() {
+  let ask = document.querySelector('a')
+  let btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  ask.addEventListener('click', () => {
+    console.log();
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+function askQuestionThenRemoveQuestion(time) {
+  question = questions[0]
+  appendQuestion(question)
+  let promise = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+      resolve(removeQuestion)
+    },time)
+  })
+  return promise
+}
